b36e85dc44d4bd3dad0179d3d2815b5374a4fc03
Originate <originated@users.noreply.github.com>
2018-01-10 15:59:28 -0800

    Add decriptive translation in setting dropdown so non-english users can actually understand it

--- src/app/components/modules/Settings.jsx
+++ src/app/components/modules/Settings.jsx
@@ -201,7 +201,7 @@ class Settings extends React.Component {
                                 <option value="fr">French</option>
                                 <option value="it">Italian</option>
                                 <option value="ko">Korean</option>
-                                <option value="zu">Chinese</option>
+                                <option value="zu">Chinese 简体中文</option>
                             </select>
                         </label>
                     </div>
